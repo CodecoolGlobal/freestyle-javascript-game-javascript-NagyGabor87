5dcc001d97fd876009061384abee18ed886ec014
Botond <63496999+B-T-ND@users.noreply.github.com>
2022-06-20 14:54:48 +0200

    add starting vectors, starting speed multiplier

--- game.js
+++ game.js
@@ -5,19 +5,20 @@ function initGame() {
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
-let ball = null;
-ball = document.getElementById("ball")
+const ball = document.getElementById("ball");
 ball.style.left = '500px';
 ball.style.bottom = '200px';
-let startY = (Math.random() - 0.5) * 5;
-let startX = (Math.random() - 0.5) * 5;
+const randomStartingVectors = [[-1,-4], [-2,-3], [-3,-3], [-4,-1], [-5,-1], [0, -3], [1, -4], [2, -3], [3, -3], [4, -1], [5, -1]];
+let startX, startY;
+let startVector = randomStartingVectors[Math.floor(Math.random()*randomStartingVectors.length)];
+let ballStartingSpeedMultiplier= 0.5;
 let ballObject = {
-    X: startX,
-    Y: startY   
+    X: startVector[0]*ballStartingSpeedMultiplier,
+    Y: startVector[1]*ballStartingSpeedMultiplier
 }
 
 function moveBall(){
     ball.style.left = parseInt((ball.style.left)) + ballObject.X + "px";
     ball.style.bottom = parseInt((ball.style.bottom)) + ballObject.Y + "px";
 }
-setInterval(() => {moveBall()}, 100);
+setInterval(() => {moveBall()}, 10);
