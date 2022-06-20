e216074d95e417ef48594a92471ada7b0f369558
Botond <63496999+B-T-ND@users.noreply.github.com>
2022-06-20 13:45:59 +0200

    started making ball movement

--- game.js
+++ game.js
@@ -5,3 +5,10 @@ function initGame() {
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
+const ball = document.querySelector("#ball");
+console.dir(ball)
+function moveBall(){
+    ball.style.left = parseInt(ball.style.left) + 40 + "px";
+    console.log(ball);
+}
+setInterval(moveBall, 1000)
